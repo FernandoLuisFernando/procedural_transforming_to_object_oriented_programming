ff482ad9f6ef1b0ebfcac8a8160d272b8721869b
Fernando Luis R. Fernando <fernandoluisr.fernando28@gmail.com>
2025-05-28 19:33:48 +0800

    defining_class_questions

---
+++ quiz_reader.py
@@ -0,0 +1,13 @@
+class questions:
+    def __init__(self, number, text, option_a, option_b, option_c, option_d, correct_answer):
+        self.number = number
+        self.text = text
+        self.option_a = option_a
+        self.option_b = option_b
+        self.option_c = option_c
+        self.option_d = option_d
+        self.answer = correct_answer
+
+   
+
+             
