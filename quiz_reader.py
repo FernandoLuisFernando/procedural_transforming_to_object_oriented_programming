dc3abc5060b3e9113bb288d897713b9bad4de79e
Fernando Luis R. Fernando <fernandoluisr.fernando28@gmail.com>
2025-05-29 09:32:39 +0800

    formats_of_the_questions_and_ans

--- quiz_reader.py
+++ quiz_reader.py
@@ -8,6 +8,15 @@ class questions:
         self.option_d = option_d
         self.answer = correct_answer
 
+    def format(self):
+        return (f"question {self.number]: {self.text}"\n")
+                self.number = number 
+                self.text = text 
+                self.option_a = option_a 
+                self.option_b = option_b 
+                self.option_c = option_c 
+                self.option_d = option_d 
+                self.correct_answer = correct_answer
    
 
              
